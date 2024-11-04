38f059e734aff3163becba23ef787f531242ccbc
ernarrr <ernar@gmail.com>
2024-11-04 16:52:27 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TV
+{
+    public void on() => Console.WriteLine("TV: ON");
+public void off() => Console.WriteLine("TV: Off");
+    public void SetChannel(int channel) => Console.WriteLine($"TV: Канал установлен на {channel}");
+
+}
+class  AudioSystem
+{
+    public void on() => Console.WriteLine("AudioSystem:On");
+    public void off() => Console.WriteLine("Audiosystem: off");
+    public void SetVolume(int volume) => Console.WriteLine("Audiosystema: gromkost na {volume");
+
+}
+ class DVDPlayer
+{
+    public void Play
+}
